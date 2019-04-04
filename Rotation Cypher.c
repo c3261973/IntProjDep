13fef45813de415082feaeba873a1b149a39697e
Lachlan Smith <lachlan.smith1846@gmail.com>
2019-04-04 05:54:09 +0000

    loose bit of a function

---
+++ Rotation Cypher.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//choose an indent style
+
+
+// stored the encryption key as linear algebraic equation (possibly will change)
+//stored xy
+
+const char stringTHEoRY[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+// have to select letter from the string
+// convert lower case to upper?
+// function will be written
+// will also make scanfunction to encrypt messages
+float Encrpytial(char leather, char quay)(%26); 
+//leather(the letter)+the value of the key mod 26(might change to 52???)
+
+// could make a menu that switches between tasks ? 
+// would that require I/O 
+//or switch through functions
+// research needed 
+// input the encryption function in a loop that needs an input to quit
+int main()    {
+    
+    printf("Enter your message to be encrypted: ", char secrets;);
+    scanf("%s", secrets);
+   
+
+}
+
+float Encryptial(char leather, char quay);
+{
+    int x, y, quay;
+    x = 3; y = 11; quay = (x + y) - 11*x;
+    (leather + key) 
+    
+}
+
