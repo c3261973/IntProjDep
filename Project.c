a1b4fd3e1534345189f81a5684044b1e1a973378
Lachlan Smith <lachlan.smith1846@gmail.com>
2019-04-04 04:36:38 +0000

    Added some formating

--- Project.c
+++ Project.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int quay = 11, leather, youlettertome;
+const char stringTHEoRY[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+// have to select letter from the string
+// function will be written
+// will also make scanfunk to encrypt messages
+float Encrpytial(leather + quay)(%26); 
+//leather(the letter)+the value of the key mod 26(might change to 52???)
 int main {
+    scanf()
+
+
+  
+}
+
 
 return 0;
 
