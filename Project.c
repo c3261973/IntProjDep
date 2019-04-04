e0c7497b6ba1b4e6de928151f3dd6fe7bb9bf5d0
Lachlan Smith <lachlan.smith1846@gmail.com>
2019-04-04 05:12:54 +0000

    listed future plans

--- Project.c
+++ Project.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
-int quay = 11, leather, youlettertome;
+int x, y, quay;
+x = 3; y = 11; quay = (x + y) - 11*x;
+// stored the encryption key as linear algebraic equation (possibly will change)
+//stored xy
+
 const char stringTHEoRY[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 // have to select letter from the string
 // function will be written
-// will also make scanfunk to encrypt messages
+// will also make scanfunction to encrypt messages
 float Encrpytial(leather + quay)(%26); 
 //leather(the letter)+the value of the key mod 26(might change to 52???)
-int main {
-    scanf()
 
-
-  
-}
+// could make a menu that switches between tasks ? 
+// would that require I/O 
+// research needed 
+// input the encryption function in a loop that needs an input to quit
+int main()    {
+    
+    printf("Enter your message to be encrypted: ", char secrets;);
+    scanf("%s", secrets);
+   
 
 
 return 0;
